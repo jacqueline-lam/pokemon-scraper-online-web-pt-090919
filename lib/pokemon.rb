47c105b93a409194516867950ec58b2b70fb9c17
Jacqueline Lam <jacqueline.karin.lam@gmail.com>
2019-11-21 00:51:38 +0000

    Automatically backed up by Learn

--- lib/pokemon.rb
+++ lib/pokemon.rb
@@ -1,2 +1,27 @@
 class Pokemon
+  attr_accessor :name, :type, :db
+  attr_reader :id
+  
+  # Pokemon initialized w/ keyword arguments of id, name, type & db
+  def initialize(id:, name:, type:, db:)
+    @id = id
+    @name = name
+    @type = type
+    @db = db  
+  end
+  
+  # Save an instance of a pokemon with the correct id
+  def self.save(name, type, db)
+    # @db = db
+    sql = <<-SQL
+      INSERT INTO pokemon (name, type)
+      VALUES (?,?)
+    SQL
+    
+    # db.execute("touch db/pokemon.db")
+    # db.execute(sql, name, type)
+    # @id = db.execute("SELECT last_insert_rowid() FROM pokemon")[0][0]
+    
+    db.execute(sql, name, type)
+  end
 end
